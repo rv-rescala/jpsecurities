271595b53868a334c752492cc50edb0f5657be31
rv <rv@rv-no-MacBook-Pro.local>
2020-12-24 00:35:20 +0900

    update

--- jpsecurities/site/rakuten.py
+++ jpsecurities/site/rakuten.py
@@ -7,24 +7,42 @@ import logging
 logger = logging.getLogger()
 
 
+class RakutenException(Exception):
+    pass
+
+
 class Rakuten:
-    def __init__(self, executable_path: str, chrome_options: Options):
+    def __init__(self, executable_path: str, chrome_options: Options, id: str, pwd: str, implicitly_wait: int = 10):
         """
 
         :param executable_path:
         :param chrome_options:
         :return:
         """
+        self.executable_path = executable_path
+        self.chrome_options = chrome_options
+        self.implicitly_wait = implicitly_wait
+        self.id = id
+        self.pwd = pwd
+
+    def __enter__(self):
         caps = DesiredCapabilities.CHROME
         caps['loggingPrefs'] = {'performance': 'INFO'}
-        self.driver: webdriver = webdriver.Chrome(executable_path=executable_path,
-                                                  chrome_options=chrome_options,
+        self.driver: webdriver = webdriver.Chrome(executable_path=self.executable_path,
+                                                  chrome_options=self.chrome_options,
                                                   desired_capabilities=caps)
+        self.driver.implicitly_wait(self.implicitly_wait)
         logger.info("Rakuten Start")
+        self.login()
 
-    def __del__(self):
+    def __exit__(self, exc_type, exc_val, exc_tb):
         self.driver.quit()
         logger.info("Rakuten End")
 
     def login(self):
-        self.driver.get(url="https://www.rakuten-sec.co.jp/web/login_error.html")
+        url = "https://www.rakuten-sec.co.jp/web/login_error.html"
+        logger.info(f"access to {url}")
+        self.driver.get(url=url)
+        self.driver.find_element_by_name("loginid").send_keys(self.id)
+        self.driver.find_element_by_name("loginid").send_keys(self.id)
+        #self.driver.find_element_by_name("btnNext").click()  # btnNext
